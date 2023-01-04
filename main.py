365125ad03e96356caebfc59204ad7e7976d5be7
Kimmo Kulmala <kimmo.kulmala@gmail.com>
2023-01-04 12:39:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# Kokeile koodia
+# HT1 - tulostaminen
+# Teksti tulostuu samalle riville
+print("Hei, vaan maailma!")
+
+# Teksti tulostuu samalle riville
+print("Hei,", "vaan", "maailma!")
+
+# Sanat tulostuvat eri riveille
+print("Hei,")
+print("vaan")
+print("maailma!")
+
+# Rivinvaihdon voi korvat end-parametrillä.
+print("1", end=" ")
+print("2", end=" ")
+print("3", end=" ")
+print("4", end=" ")
+
+# Tyhjä rivinvaihto
+print("")
+
+# Tulostuksessa välimerkin voi korvata
+print("Alaviiva","korvaa","välilyönnin.",sep="_")
+
+# Nimesi
+print("" , end=" ")
+#Ikäsi
+print("")
+# Luokkasi ja koulusi
+print("","")
+# Eniten pidän ....
+print("","",sep="")
+# Minusta paras sosiaalinen media on...
+print("")
